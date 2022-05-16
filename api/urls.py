060fd462d25aeb606170be8df5cefca3ad39d7a6
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-05-16 12:17:08 -0500

    Simple jwt authentication added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-    path("test/", views.test, name="test")
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("create/assignment", views.create_assignment, name="create_assignment"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(["GET"])
-def test():
-    data = {
-        "message": "Hello World"
-    }
-    return Response(data)
+@api_view(["POST"])
+def create_assignment(request):
+    print(request.data)
+    return Response({"message": "create_assignment"})
+    
 
 
 # Create your views here.
