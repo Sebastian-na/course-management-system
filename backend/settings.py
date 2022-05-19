de08a76ba2fb0644ebbf796e98dfc5b0157d6cae
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-05-18 20:13:47 -0500

    modified static configuration

--- backend/settings.py
+++ backend/settings.py
@@ -121,13 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
-MEDIA_URL = "/files/"
-MEDIA_ROOT = 'static/files'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = 'static/media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 
 # Default primary key field type
